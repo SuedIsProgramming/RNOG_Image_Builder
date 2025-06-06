c3ac10ae6e8d16c4f8b32e943c77fed53c327f6d
ssued <ssued24@gmail.com>
2025-06-05 22:39:11 -0400

    Modify step 1/2 to look like tutorial

--- simulation/1_generate_neutrinos.py
+++ simulation/1_generate_neutrinos.py
@@ -2,12 +2,16 @@ from __future__ import absolute_import, division, print_function
 from NuRadioReco.utilities import units
 from NuRadioMC.EvtGen.generator import generate_eventlist_cylinder
 
-# define simulation volume
+# Setup logging
+from NuRadioReco.utilities.logging import _setup_logger
+logger = _setup_logger(name="")
+
+# define simulation volume (artificially close by to make them trigger)
 volume = {
-'fiducial_zmin':-2.7 * units.km,  # the ice sheet at South Pole is 2.7km deep
+'fiducial_zmin':-1 * units.km,  # the ice sheet at South Pole is 2.7km deep
 'fiducial_zmax': 0 * units.km,
 'fiducial_rmin': 0 * units.km,
-'fiducial_rmax': 4 * units.km}
+'fiducial_rmax': 1 * units.km}
 
-# generate one event list at 1e19 eV with 1000 neutrinos (100 for now for debugging)
-generate_eventlist_cylinder('neutrinos_1e19_100n.hdf5', 1e2, 1e19 * units.eV, 1e19 * units.eV, volume)
+# generate one event list at 1e19 eV with 100 neutrinos
+generate_eventlist_cylinder('1e19_n1e3.hdf5', 1e2, 1e19 * units.eV, 1e19 * units.eV, volume)
