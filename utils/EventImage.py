59d4b094b2c2fed225d6fe371eb949c475153a72
ssued <ssued24@gmail.com>
2025-06-20 10:46:29 -0400

    Add json support for importing detector.

--- utils/EventImage.py
+++ utils/EventImage.py
@@ -1,6 +1,7 @@
 import NuRadioReco.modules.io.eventReader
 from matplotlib import pyplot as plt
 import numpy as np
+import json
 
 def bin(voltage,time,nbins,bin_method='mean'):
     total_time = time[-1] - time[0]
@@ -28,25 +29,33 @@ class EventImage:
         self.nbins = nbins
         self.tot_time = tot_time 
 
-        event_reader = NuRadioReco.modules.io.eventReader.eventReader()
-        event_reader.begin(nur_file, read_detector=True)
-        det = event_reader.get_detector() # Obtain detector description from .nur file
-        if det is not None:
-            nchannels = len(det._channels)
-            nstations = len(det._stations)
-        else:
-            raise ValueError("Detector object is None. Cannot access '_channels' or '_stations'.")
-        self.nrows = nchannels * nstations # Number of rows will be the total number of channels for all stations
+        try: # Tries reading .nur file to obtain detector.
+            event_reader = NuRadioReco.modules.io.eventReader.eventReader()
+            event_reader.begin(nur_file, read_detector=True)
+            det = event_reader.get_detector()  # Obtain detector description from .nur file
+            if det is not None:
+                self.nchannels = len(det._channels)
+                self.nstations = len(det._stations)
+                self.nrows = self.nchannels * self.nstations # Number of rows will be the total number of channels for all stations
+            else:
+                raise ValueError("Detector object is None. Cannot access '_channels' or '_stations'.")
+        except Exception as e: # If an issue occurs, it will then simply try to read json.
+            print('Issue encountered reading the .nur file, will read json instead')
+            with open('simulation/multistation.json', 'r') as f:
+                det = json.load(f)
+                self.nchannels = len(det['channels'])
+                self.nstations = len(det['stations'])
+                self.nrows = self.nchannels * self.nstations
+
         self.ncols = nbins
         self.bin_time = tot_time / nbins
-
         for iE, event in enumerate(event_reader.run()):
             event_arr = np.zeros((self.nrows,self.ncols)) # Initialize an empty matrix
             for iStation, station in enumerate(event.get_stations()):
                 for iChannel, channel in enumerate(station.iter_channels()):
                     volts_hilb = channel.get_hilbert_envelope()
                     times = channel.get_times()
-                    
+
 
 # For debugging
 eimg = EventImage('/data/condor_shared/users/ssued/RNOG_Image_Builder/simulation/output.nur',24)
