2118276ef0c88d0cd58c133c97e1428b2ea646a8
ssued <ssued24@gmail.com>
2025-06-24 18:06:59 -0400

    Change simulation z to -3km

--- simulation/1_generate_neutrinos.py
+++ simulation/1_generate_neutrinos.py
@@ -8,10 +8,10 @@ logger = _setup_logger(name="")
 
 # define simulation volume (artificially close by to make them trigger)
 volume = {
-'fiducial_zmin':-1 * units.km,  # the ice sheet at South Pole is 2.7km deep
+'fiducial_zmin':-3 * units.km,  # the ice sheet at South Pole is 2.7km deep
 'fiducial_zmax': 0 * units.km,
 'fiducial_rmin': 0 * units.km,
 'fiducial_rmax': 1 * units.km}
 
 # generate one event list at 1e19 eV with 100 neutrinos
-generate_eventlist_cylinder('1e19_n1e3.hdf5', 1e2, 1e19 * units.eV, 1e19 * units.eV, volume)
+generate_eventlist_cylinder('1e19_n1e3.hdf5', 1e3, 1e19 * units.eV, 1e19 * units.eV, volume)
