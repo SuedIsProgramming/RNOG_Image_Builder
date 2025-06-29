6683c3c64ab43f4748ea1dff23f0d88344e74998
ssued <ssued24@gmail.com>
2025-06-29 18:15:37 -0400

    Add filepath automatization and modularity.

--- simulation/3_generate_images.py
+++ simulation/3_generate_images.py
@@ -1,24 +1,69 @@
 from matplotlib import pyplot as plt
-import NuRadioReco.modules.io.eventReader
+from utils.my_utils import get_unique_events, get_rel_dir
+import numpy as np
+import json
 
-event_reader = NuRadioReco.modules.io.eventReader.eventReader()
+def _bin(voltage,time,nbins,bin_method='mean'):
+    total_time = time[-1] - time[0]
+    bin_width = total_time / nbins
+    bin_edges = np.arange(time[0],time[-1] + bin_width, bin_width)
+    #bin_centers = (bin_edges[:-1] + bin_edges[1:]) / 2 Is there any use of having the centers?
 
-file = 'output.nur'
-event_reader.begin(file)
+    bin_indices = np.digitize(time, bin_edges)
 
-events_unique = []
-iFirst = -1
-first_event = None
-for event in event_reader.run():
-    primary = event.get_primary()
-    iP = primary.get_id()
-    event.set_id(iP)
-    if iFirst is not event.get_id():
-        if first_event is not None:
-            events_unique.append(first_event)
-        iFirst = event.get_id()
-        first_event = event
-    if event is not first_event:    
-        first_event.set_station(event.get_station())
+    bin_voltage = np.zeros(len(bin_edges) - 1)
+
+    for i in range(1, len(bin_edges)):
+        voltages_in_bin = voltage[bin_indices == i]
+        if voltages_in_bin.size > 0:
+            if bin_method == 'max':
+                bin_voltage[i - 1] = np.max(voltages_in_bin)
+            elif bin_method == 'mean':
+                bin_voltage[i - 1] = np.mean(voltages_in_bin)
+
+    return bin_voltage, (time[0],time[-1])
+
+rel_dir = get_rel_dir()
+events_unique = get_unique_events(f'{rel_dir}/output.nur')
+
+TOT_TIME = 6000
+N_BINS = 1024
+
+json_file = f'{rel_dir}/multistation.json'
+with open(json_file, 'r') as f:
+            print(f'Issue encountered reading the .nur file, will read from {json_file} instead')
+            with open(json_file, 'r') as f:
+                det = json.load(f)
+                N_CHANNELS = len(det['channels'])
+                N_STATIONS = len(det['stations']) # Assumes stations are all the same.
+
+album = []
+
+for event in events_unique:
+    stations = list(event.get_stations()) #MIGHT NOT NEED THIS
+    # stations_num = len(stations)
+    # # Get the number of channels for each station (assuming all stations have the same number of channels)
+    # channels_per_station = [len(list(station.iter_channels())) for station in stations]
+    TOT_CHANNELS = N_CHANNELS * N_STATIONS # sum(channels_per_station)
+
+    min_time = min(
+        channel.get_times()[0]
+        for station in stations
+        for channel in station.iter_channels()
+    )
+
+    image = np.zeros((TOT_CHANNELS, N_BINS))
+    
+    for station in stations:
+        for channel in station.iter_channels():
+            time_arr = np.array(channel.get_times())
+            time_arr = time_arr - min_time
+            # print(time_arr)
+
+
+
+
+
+
     
 
--- simulation/3_generate_traces_multi.py
+++ simulation/3_generate_traces_multi.py
@@ -1,64 +1,17 @@
-import NuRadioReco.modules.io.eventReader
+from utils.my_utils import get_unique_events, get_rel_dir
 from matplotlib import pyplot as plt
-# from utils.my_utils import get_unique_events
+from typing import List
 
-plt.style.use('dark_background')
+#plt.style.use('dark_background')
 
-def get_unique_events(fPath):
-    """
-    Reads a NuRadioReco .nur file and returns a list of unique events, 
-    where each event contains all stations that detected the same primary particle.
+rel_dir = get_rel_dir() 
 
-    This function iterates through all events in the file, groups together 
-    stations that correspond to the same primary particle (using the primary particle ID), 
-    and constructs a list of unique events, each with all associated stations.
-
-    Parameters
-    ----------
-    fPath : str
-        Path to the .nur file to be read.
-
-    Returns
-    -------
-    events_unique : list of NuRadioReco.framework.event.Event
-        List of unique events, each containing all stations that saw the same primary particle.
-
-    Notes
-    -----
-    - The function assumes that the eventReader yields events in order of primary particle ID.
-    - For each new primary particle ID, a new event is started and stations are added to it.
-    - The function will output events with event ID equal to the particle ID they correspond to.
-    """
-    event_reader = NuRadioReco.modules.io.eventReader.eventReader()
-    event_reader.begin(fPath)
-    events_unique = []
-    iFirst = -1
-    first_event = None
-    for iE, event in enumerate(event_reader.run()):
-        primary = event.get_primary()
-        iP = primary.get_id()
-        event.set_id(iP)
-
-        # Start a new unique first event if the primary ID changes
-        if iFirst != event.get_id():
-            if first_event is not None:
-                events_unique.append(first_event)
-            iFirst = event.get_id()
-            first_event = event
-        # Add stations from the same primary to the current first event
-        if event is not first_event:    
-            first_event.set_station(event.get_station())
-
-    return events_unique
-
-events_unique = get_unique_events('output.nur')
+events_unique = get_unique_events(f'{rel_dir}/output.nur')
 
 if len(events_unique) == 0:
     print('No events detected.')
 
 for iE, event in enumerate(events_unique):
-    if iE != 0:
-        break
     stations = list(event.get_stations())
     stations_num = len(stations)
     # Get the number of channels for each station (assuming all stations have the same number of channels)
@@ -75,25 +28,25 @@ for iE, event in enumerate(events_unique):
             volts = ch.get_trace()
             times = ch.get_times()
             tot_time = int(times[-1] - times[0]) + 1
-            axs[ax_idx].plot(times, volts, label=f'Station {station.get_id()} Channel {ch.get_id()}', linewidth=1.5)
-            axs[ax_idx].set_title(
+            axs[ax_idx].plot(times, volts, label=f'Station {station.get_id()} Channel {ch.get_id()}', linewidth=1.5) # type:ignore
+            axs[ax_idx].set_title( # type:ignore
                 rf"Event {event.get_id()} | Station {station.get_id()} | Channel {ch.get_id()} | $t_{{trigger}} = {tot_time}$ ns",
                 fontsize=11, fontweight='bold'
             )
-            axs[ax_idx].set_ylabel("Voltage [V]", fontsize=10)
-            axs[ax_idx].legend(loc=4, fontsize=9)
-            axs[ax_idx].grid(True, which='both', linestyle='--', linewidth=0.7, alpha=0.7)
+            axs[ax_idx].set_ylabel("Voltage [V]", fontsize=10) # type:ignore
+            axs[ax_idx].legend(loc=4, fontsize=9) # type:ignore
+            axs[ax_idx].grid(True, which='both', linestyle='--', linewidth=0.7, alpha=0.7) # type:ignore
             ax_idx += 1
 
-    axs[-1].set_xlabel("Time [ns]", fontsize=11)
+    axs[-1].set_xlabel("Time [ns]", fontsize=11) # type:ignore
     for ax in axs:
-        ax.tick_params(axis='both', which='major', labelsize=9)
-        ax.set_facecolor('#f7f7f7')
+        ax.tick_params(axis='both', which='major', labelsize=9) # type:ignore
+        ax.set_facecolor('#f7f7f7') # type:ignore
 
     station_ids = [station.get_id() for station in stations]
     print(f'Saving trace for particle {event.get_id()}, detected at Stations: {station_ids}')
     fig.suptitle(f"Event {event.get_id()} | Detected at {stations_num} Stations", fontsize=14, fontweight='bold')
-    fig.savefig(f"particle_{event.get_id()}_detected_at_{stations_num}_stations.png", dpi=150)
+    fig.savefig(f"{rel_dir}/particle_{event.get_id()}_detected_at_{stations_num}_stations.png", dpi=150)
     plt.close(fig)
 
 ################################## OLD ###################################################
